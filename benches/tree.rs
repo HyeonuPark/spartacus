7035e8e07a4c3d67b1b4836792901b6e5ec3d9f8
Hyeonu Park <nemo1275@gmail.com>
2018-01-08 19:55:15 +0900

    add copyright reference for benchmark code

--- benches/tree.rs
+++ benches/tree.rs
@@ -16,6 +16,19 @@ use spartacus::arena::vec_arena::{VecArena, Bucket};
 
 use spartacus::tree::{TreeMap, Noop, RevTreap};
 
+// Test macros are copied from rust-lang repository
+// https://github.com/rust-lang/rust/blob/9bea79bd5ef492cf2c24e098ac93638446cb4860/src/liballoc/benches/btree/map.rs
+
+// Copyright 2017 The Rust Project Developers. See the COPYRIGHT
+// file at the top-level directory of this distribution and at
+// http://rust-lang.org/COPYRIGHT.
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+
 macro_rules! map_insert_rand_bench {
     ($name: ident, $n: expr, $map: ty) => (
         #[bench]
